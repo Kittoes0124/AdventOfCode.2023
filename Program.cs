b719863217582e8ebb9ccdb22152595b69f21cb9
David Jürgens <kittoes@byteterrace.com>
2023-12-03 22:59:42 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using AdventOfCode2023;
+
+foreach (var i in Enumerable.Range(count: 1, start: 0)) {
+    switch (i) {
+        case 0:
+            Console.WriteLine(value: $"Day 1: {Day1.RunPart1()}, {Day1.RunPart2()}");
+            break;
+        default:
+            throw new NotImplementedException();
+    }
+}
