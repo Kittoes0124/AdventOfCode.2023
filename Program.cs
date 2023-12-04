265e4a29ceb7ba4d528c733e8d0cba483c412dbf
David Jürgens <kittoes@byteterrace.com>
2023-12-04 00:02:27 -0600

    added day 2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using AdventOfCode2023;
 
-foreach (var i in Enumerable.Range(count: 1, start: 0)) {
+foreach (var i in Enumerable.Range(count: 2, start: 0)) {
     switch (i) {
         case 0:
             Console.WriteLine(value: $"Day 1: {Day1.RunPart1()}, {Day1.RunPart2()}");
+            break;
+        case 1:
+            Console.WriteLine(value: $"Day 2: {Day2.RunPart1()}, {Day2.RunPart2()}");
             break;
         default:
             throw new NotImplementedException();
